16d09de318f5f653ae7f6fc4115936de23683403
Jonathan <jonathan.heirbaut.1@student.kdg.be>
2014-02-05 10:53:09 +0100

    Test udp packets in comment

--- Android/src/main/java/be/kdg/FastradaMobile/controllers/InputDataController.java
+++ Android/src/main/java/be/kdg/FastradaMobile/controllers/InputDataController.java
@@ -1,5 +1,11 @@
 package be.kdg.FastradaMobile.controllers;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 /**
  * Created by FezzFest on 4/02/14.
  */
@@ -10,9 +16,17 @@ public class InputDataController {
         return 20;
     }
 
-    public byte[] receiveUdpPacket(String s, String s1) {
-        byte[] result = {};
-
+    public byte[] receiveUdpPacket(String strAddress, int port) {
+        byte[] result = new byte[100];
+        try {
+            InetAddress address = InetAddress.getByName(strAddress);
+            DatagramPacket packet = new DatagramPacket(result, result.length, address, port);
+            DatagramSocket datagramSocket = new DatagramSocket();
+            datagramSocket.setSoTimeout(5000);
+            datagramSocket.receive(packet);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 }
--- Android/test/be/kdg/FastradaMobile/TestInputDataController.java
+++ Android/test/be/kdg/FastradaMobile/TestInputDataController.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Random;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -15,19 +17,55 @@ import static org.junit.Assert.assertEquals;
  * Created by Jonathan on 5/02/14.
  */
 public class TestInputDataController {
-    @Test
-    public void testReceiveUdpPacket() throws IOException {
-       /* InputDataController inputDataController = new InputDataController();
-        byte[] arrayStream = "test".getBytes();
-        InetAddress address = InetAddress.getByName("127.0.0.1");
-        DatagramPacket packet = new DatagramPacket(arrayStream, arrayStream.length, address, 9000);
-        DatagramSocket datagramSocket = new DatagramSocket();
-        datagramSocket.send(packet);
-        byte[] result = inputDataController.receiveUdpPacket("127.0.0.1", "9000");
 
-        assertArrayEquals("Send and reveive bytes must be the same", result, "test".getBytes());*/
+    private void sendUdpPackets(final byte[] packet) {
+        Thread thread = new Thread() {
+            public void run() {
+                byte[] arrayStream = packet;
+                try {
+                    DatagramSocket datagramSocket = new DatagramSocket();
+                    while (true) {
+                        InetAddress address = InetAddress.getByName("127.0.0.1");
+                        DatagramPacket packet = new DatagramPacket(arrayStream, arrayStream.length, address, 9000);
+                        datagramSocket.send(packet);
+                        Thread.sleep(500);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+
+    @Test
+    public void testReceiveFixedUdpPacket() throws IOException {
+  /*      sendUdpPackets("test".getBytes());
+
+        InputDataController inputDataController = new InputDataController();
+        byte[] result = inputDataController.receiveUdpPacket("127.0.0.1", 9000);
+
+        assertArrayEquals("Send and reveive bytes must be the same", "test".getBytes(), result);
+*/
+    }
+
+
+    @Test
+    public void testReceiveVariableUdpPacket() throws IOException {
+     /*   Random random = new Random();
+        byte[] bytes = new byte[30];
+       random.nextBytes(bytes);
+        sendUdpPackets(bytes);
+
+        InputDataController inputDataController = new InputDataController();
+        byte[] result = inputDataController.receiveUdpPacket("127.0.0.1", 9000);
+
+        assertArrayEquals("Variable receive udp bytes must be the same", bytes, result);*/
 
     }
+
 
     @Test
     public void testEngineMsgParser() {
