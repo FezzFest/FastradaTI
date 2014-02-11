395c851bb77040b4ab28ac97164d5880e4cf51d4
Philip Vissenaekens <philipvissenaekens@msn.com>
2014-02-11 16:52:25 +0100

    changed android ui to show RPM in green, orange, red and gray values.

--- Android/src/main/java/be/kdg/FastradaMobile/activities/MainActivity.java
+++ Android/src/main/java/be/kdg/FastradaMobile/activities/MainActivity.java
@@ -1,13 +1,19 @@
 package be.kdg.FastradaMobile.activities;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import be.kdg.FastradaMobile.R;
 import org.codeandmagic.android.gauge.GaugeView;
 
 public class MainActivity extends Activity
 {
+    private final static int rpmLimiter= 8000;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
@@ -30,5 +36,93 @@ public class MainActivity extends Activity
         // Temperature indicator
         TextView tempIndicator = (TextView) findViewById(R.id.dashboard_temperature_units);
         tempIndicator.setText("103 °C");
+
+        showRPM(1);
+    }
+
+    public void showRPM(int rpm){
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearImageView);
+        int numberOfGreens, numberOfYellows;
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            numberOfGreens = 14;
+            numberOfYellows = 3;
+        } else {
+            numberOfGreens = 8;
+            numberOfYellows = 2;
+        }
+        int numberOfLeds = linearLayout.getChildCount();
+        for(int i=0; i< numberOfLeds; i++) {
+            ImageView imageView = (ImageView) linearLayout.getChildAt(i);
+            if (i<numberOfGreens) {
+                //green
+                if(rpm>(rpmLimiter/ numberOfLeds)*i){
+                    imageView.setImageResource(R.drawable.rpmgreen);
+                }
+                else{
+                    imageView.setImageResource(R.drawable.rpmgray);
+                }
+            }else if(i<numberOfGreens+numberOfYellows){
+                //orange
+                if(rpm>(rpmLimiter/ numberOfLeds)*i){
+                    imageView.setImageResource(R.drawable.rpmyellow);
+                }
+                else{
+                    imageView.setImageResource(R.drawable.rpmgray);
+                }
+            } else {
+                //red
+                if(rpm>(rpmLimiter/ numberOfLeds)*i){
+                    imageView.setImageResource(R.drawable.rpmred);
+                }
+                else{
+                    imageView.setImageResource(R.drawable.rpmgray);
+                }
+            }
+        }
+
+
+
+
+//        LinearLayout linearImageView = (LinearLayout) findViewById(R.id.linearImageView);
+//       if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+//           int imgWidth = (linearImageView.getWidth()/20)-5;
+//         for(int i = 1; i<=20;i++){
+//             ImageView imageView = new ImageView(this);
+//             if (i<=14) {
+//                 //green
+//                 if(rpm>=(rpmLimiter/20)*i){
+//                     imageView.setImageResource(R.drawable.rpmgreen);
+//                 }
+//                 else{
+//                     imageView.setImageResource(R.drawable.rpmgray);
+//                 }
+//             }else if(i<=17){
+//                 //orange
+//                 if(rpm>=(rpmLimiter/20)*i){
+//                     imageView.setImageResource(R.drawable.rpmyellow);
+//                 }
+//                 else{
+//                     imageView.setImageResource(R.drawable.rpmgray);
+//                 }
+//             } else {
+//                 //red
+//                 if(rpm>=(rpmLimiter/20)*i){
+//                     imageView.setImageResource(R.drawable.rpmred);
+//                 }
+//                 else{
+//                     imageView.setImageResource(R.drawable.rpmgray);
+//                 }
+//             }
+//
+//             imageView.setMaxHeight(imgWidth);
+//             imageView.setMaxWidth(imgWidth);
+//             imageView.setAdjustViewBounds(true);
+//             ActionBar.LayoutParams()
+//             imageView.setLayoutParams();
+//         }
+//       }
+//        else{
+//
+//       }
     }
 }
