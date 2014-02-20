ee78563b9f0fd9e5694b206959252dce481dc260
pjpauwels <Moppentapper1>
2014-02-21 00:57:04 +0100

    working spring rest example
    Working rest service for integer list (sessionid's by example)

---
+++ FastradaWeb/src/main/java/app/Parameter.java
@@ -0,0 +1,8 @@
+package app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parameter {
+    public int value;
+}
--- FastradaWeb/src/main/java/app/SessionController.java
+++ FastradaWeb/src/main/java/app/SessionController.java
@@ -1,18 +1,8 @@
 package app;
 
-import org.codehaus.jackson.map.util.JSONPObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriTemplate;
-
-import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,12 +12,19 @@ public class SessionController {
 
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody String list() {
+    @ResponseBody
+    public List<Parameter> list() {
         //TODO get from database
 
-        return "{1,2,3,4,5,6,7,8,9}";
-    }
+        List<Parameter> parameters = new ArrayList<Parameter>();
+        for (int teller = 0; teller < 10; teller++) {
+            Parameter x = new Parameter();
+            x.value = teller*25;
+            parameters.add(x);
+        }
 
+        return parameters;
+    }
 
     @RequestMapping(value = "/{id}/{parameter}", method = RequestMethod.GET)
     public HashMap<Integer, Integer> getParameterBySessionId(int sessionId, String parameter) {
